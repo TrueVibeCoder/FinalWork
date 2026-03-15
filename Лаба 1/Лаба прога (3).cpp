be6e7292ff874141155532215819a46fdd1970db
Admin <admin@example.com>
2026-03-15 15:10:35 +0300

    Добавка по проге

---
+++ Лаба 1/Лаба прога (3).cpp
@@ -0,0 +1,34 @@
+// Лабораторная работа №1
+// Выполнил: Флеонов М.В.
+
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+    double x, s1, s2, s3, y;
+
+    printf("Введите значение x: ");
+    scanf("%lf", &x);
+    
+    s1=x;
+
+    s2 = fabs(x) + 2 * tan(x);
+
+    s3 = 5.78 * exp(x - 1);
+
+    y = s2 / s3;
+
+    // Вывод значений
+    printf("\n--- Исходные данные ---\n");
+    printf("s1 = %lf\n", s1);
+    printf("s2 = |x| + 2*tan(x) = %lf\n", s2);
+    printf("s3 = 5.78 * exp(x - 1) = %lf\n", s3);
+
+    printf("\n--- Результат ---\n");
+    printf("y = s2 / s3 = %.6lf\n", y);
+
+	getch ();
+    return 0;	
+}
